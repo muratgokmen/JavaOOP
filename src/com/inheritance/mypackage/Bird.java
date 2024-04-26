cb560af7befec1e2be4401eb8d174c429a974a7b
muratgokmen <59473220+muratgokmen@users.noreply.github.com>
2024-04-26 14:29:21 +0300

    inheritance example completed

--- src/com/inheritance/mypackage/Bird.java
+++ src/com/inheritance/mypackage/Bird.java
@@ -1,5 +1,13 @@
 package com.inheritance.mypackage;
 
-public class Bird {
-
+public class Bird extends Animal {
+	
+	String movementType = "Birds fly";
+	
+	//This method overrided because this feature is different other animals.
+	@Override
+	String getMovementType() {
+		return movementType;
+	}
+	
 }
--- src/com/inheritance/mypackage/Dog.java
+++ src/com/inheritance/mypackage/Dog.java
@@ -1,5 +1,13 @@
 package com.inheritance.mypackage;
 
-public class Dog {
+public class Dog extends Animal {
+	
+	String dogSpeakVoiceNaming = "Bark";
+	
+	// Just dogs bark 
+	@Override
+	void speak() {
+		System.out.println("Dogs is speak voice like : " + dogSpeakVoiceNaming);;
+	}
 
 }
