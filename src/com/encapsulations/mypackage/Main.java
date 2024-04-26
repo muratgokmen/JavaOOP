626cae1b6ea7bed4f397256248d81130fc44d295
muratgokmen <59473220+muratgokmen@users.noreply.github.com>
2024-04-27 01:02:20 +0300

    encapsulation example completed

---
+++ src/com/encapsulations/mypackage/Main.java
@@ -0,0 +1,16 @@
+package com.encapsulations.mypackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Book book = new Book();
+		
+		// book.id = 10;   book.id is not visible . Because is modifier is private. 
+		
+		// To access or modify private fields, we provide public getter and setter methods
+		book.setId(5);
+		book.getId();
+	}
+
+}
